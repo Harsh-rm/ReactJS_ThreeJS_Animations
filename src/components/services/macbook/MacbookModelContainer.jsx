8313264eb57d0866764d4e92ad8b4522528dde1a
Harsh-rm <harshu98@gmail.com>
2025-06-18 17:03:16 -0700

    Edited the Macbook screen thumbnail in services section

--- src/components/services/macbook/MacbookModelContainer.jsx
+++ src/components/services/macbook/MacbookModelContainer.jsx
@@ -1,14 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { MacbookModel } from "./MacbookModel";
+import { MacbookModelEdited } from "./MacbookModelEdited";
 import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
 
 const MacbookModelContainer = () => {
   return (
     <Canvas>
       <Suspense fallback="loading...">
-        <Stage environment="apartment" intensity={0.5}>
-          <MacbookModel />
+        <Stage environment="sunset" intensity={0.01}>
+          <MacbookModelEdited />
         </Stage>
         <OrbitControls enableZoom={false} autoRotate/>
         <PerspectiveCamera position={[-1,0,1.8]} zoom={0.8} makeDefault/>
