face0f5461d93c0ae141317291470296a1fd78c5
Harsh-rm <harshu98@gmail.com>
2025-06-19 09:44:19 -0700

    Adding contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Hero from "./components/hero/Hero";
 import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
@@ -11,6 +13,16 @@ const App = () => {
 
     <section id="#services">
       <Services />
+    </section>
+
+    {/*
+    <section id="#portfolio">
+      <Portfolio />
+    </section>
+    */}
+    
+    <section id="#contact">
+      <Contact />
     </section>
 
     </>
